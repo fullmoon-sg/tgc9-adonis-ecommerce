162fa051615ab199d13c7a29a8cc27ed3a753676
fullmoon-sg <mbfoo7757@gmail.com>
2021-01-08 04:05:44 +0000

    able to get the key

---
+++ oldbooks/app/Controllers/Http/CloudinaryController.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const Cloudinary=use('Cloudinary')
+const Config = use('Config')
+const apiSecret=Config.get('cloudinary.api_Secret')
+
+class CloudinaryController {
+  sign({request}){
+    let data = request.get();
+    //will receive cloudinary/sign?params_to_sign=xxxx
+    let toSign = JSON.parse(data.params_to_sign);
+    return Cloudinary.utils.api_sign_request(toSign,apiSecret);
+   }
+}
+module.exports = CloudinaryController
